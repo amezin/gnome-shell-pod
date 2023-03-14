1ef00535df98281aca6ff513516644e7ad3eceff
Aleksandr Mezin <mezin.alexander@gmail.com>
2023-03-14 20:11:31 +0200

    Workaround for hang on Arch Linux

--- common/usr/local/bin/wait-user-bus.sh
+++ common/usr/local/bin/wait-user-bus.sh
@@ -3,7 +3,9 @@
 set -ex
 
 busctl --system --watch-bind=true status >/dev/null
-systemctl is-system-running --wait
+
+# systemctl is-system-running --wait hangs on Arch Linux
+timeout 10s systemctl is-system-running --wait || systemctl is-system-running --wait
 
 while ! busctl --user --watch-bind=true status >/dev/null; do
     sleep 0.1
