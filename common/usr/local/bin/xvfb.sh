380192fdb5c2c2497eb013cddede77560c6b9b89
Aleksandr Mezin <mezin.alexander@gmail.com>
2023-08-18 02:00:23 +0300

    xvfb: track when xvfb is actually ready to accept connections

---
+++ common/usr/local/bin/xvfb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/185
+# sd_notify() was added to Xorg in 21.1
+# But some distros still ship 1.20.* - that's why this "forking" wrapper exists
+
+read -r DISPLAY_NUMBER < <(
+    if [ -n "${PIDFILE}" ]; then
+        echo "${BASHPID}" >"${PIDFILE}"
+    fi
+
+    exec Xvfb "$@" -displayfd 1
+)
